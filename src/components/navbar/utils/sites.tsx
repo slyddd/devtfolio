ac8cd0bf9fcfb7a8b21e35686a189a36bc0dc7c9
slyddd <dayvidnavarrete@gmail.com>
2024-12-04 17:43:34 -0500

    remove the unused icons folder

--- src/components/navbar/utils/sites.tsx
+++ src/components/navbar/utils/sites.tsx
@@ -1,21 +1,14 @@
-import { Home } from "../icons/home.tsx";
-import { Project } from "../icons/project.tsx";
-import { Blog } from "../icons/blog.tsx";
-
 export const sites = [
   {
     name: "Home",
     href: "/",
-    icon: <Home />,
   },
   {
     name: "Projects",
     href: "#projects",
-    icon: <Project />,
   },
   {
     name: "Blog",
     href: "#blog",
-    icon: <Blog />,
   },
 ];
